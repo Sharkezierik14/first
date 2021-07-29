40bcbd3cba13075932839e531824de18f2cc4490
Artem Dubenko <artyom.dubenko@gmail.com>
2021-07-29 22:42:38 +0300

    clarified the task and added some configuration to run it in VS Code

--- main.cpp
+++ main.cpp
@@ -1,51 +1,51 @@
 #include <iostream>
-
+#include <vector>
+#include <assert.h>
 
 using namespace std;
 
-void isMatch(int[],int &,int &,int &);
+// PROBLEM: given a list of numbers from 1..n
+// listed in a random order, find the one
+// single number that is missing from the list.
+//
+// INSTRUCTIONS: implement the `findMissing`
+// function, do not touch anything else,
+// do not modify the parameters in this function.
+//
+// When you're done, run the app, all 3 tests
+// should pass without an error.
+int findMissing(const vector<int>& numbers)
+{
+    return 1; // returns `1` as the missing number, but it's just a fake example
+}
+
+void test1()
+{
+    vector<int> numbers {1, 2, 3, 4, 6};
+    int missing = findMissing(numbers);
+
+    assert(missing == 5);
+}
+
+void test2()
+{
+    vector<int> numbers {2, 3, 4, 5};
+    int missing = findMissing(numbers);
+
+    assert(missing == 1);
+}
+
+void test3()
+{
+    vector<int> numbers {1, 2, 3, 4, 5};
+    int missing = findMissing(numbers);
+
+    assert(missing == 6);
+}
 
 int main()
 {
-    int t[10]={15,2,5,7,9,13,15,21,4,6};
-    int db=10;
-    // text
-    int mini = t[0];
-    int maxi = t[0];
-    for (int i=0;i<10;i++)
-        {
-            if (t[i] < mini)
-            {
-                mini=t[i];
-            }
-            else if (t[i] > maxi )
-            {
-                maxi=t[i];
-            }
-        }
-   // cout<<mini<<endl;
-   // cout<<maxi<<endl;
-    isMatch(t,db,mini,maxi);
-
-}
-
-void isMatch(int t[],int & db, int & mini, int & maxi)
-{
-
-    int j=0;
-   for (int i = mini; i < maxi; i++)
-   {
-       for (j=0; j<10;j++)
-       {
-           if (i==t[j])
-           {
-               break;
-           }
-       }
-       if (j > 9 )
-           {
-               cout<<i<<endl;
-           }
-   }
-
+    test1();
+    test2();
+    test3();
 }
